19089b320226645cc2b8c2dd8bd5b344ea598cad
Jaime Vicente Jr <jdeveloperweb@yahoo.com.br>
2026-03-05 15:32:34 -0300

    fix

--- bizu-backend/src/main/java/com/bizu/portal/student/api/QuestController.java
+++ bizu-backend/src/main/java/com/bizu/portal/student/api/QuestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/student/quests")
+@RequestMapping("/api/v1/student/quests")
 @RequiredArgsConstructor
 public class QuestController {
 
